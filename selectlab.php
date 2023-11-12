8dbeb1e72720ce38e83f618868ecf0b361bbb251
Akhil Babu MK <akhilbabu633@gmail.com>
2023-11-12 17:35:59 +0530

    selected lab questions are listed successfully

--- selectlab.php
+++ selectlab.php
@@ -22,8 +22,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Retrieve username and password from the form
     
     $_SESSION['labid'] = $_POST['selectedLab'];
-    echo $_SESSION['labid'];
+    $currentLab =  $_SESSION['labid'];
+    echo "current lab is " ,$currentLab;
+
+    // Fetch questions associated with the selected lab
+    $sqlQuestions = "SELECT * FROM Question WHERE LabID = $currentLab";
+    $resultQuestions = $conn->query($sqlQuestions);
+
+    if (!$resultQuestions) {
+        // Handle the case where the query fails
+        echo "Error: " . $sqlQuestions . "<br>" . $conn->error;
     }
+    
+}
    else{
    echo "world";
    }
@@ -41,7 +52,7 @@ if (!isset($_SESSION['username'])) {
 }
 
 // Debug statement
-echo "After checking userID. UserID: " . $_SESSION['userID'];
+echo "After checking userID. UserID: " . $_SESSION['userid'];
 
 // Include the database connection or any necessary configuration files
 // require_once('config.php');
@@ -87,10 +98,13 @@ if ($result) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Select Lab</title>
+    <!-- Include Bootstrap CSS link or CDN here -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
 
-<div>
+<div class= "container mt-5">
+
     <h2>Select a Lab</h2>
 
     <!-- Form to select a lab -->
@@ -109,7 +123,34 @@ if ($result) {
 
         <button type="submit">Select Lab</button>
     </form>
+
+    <?php
+    // Display questions only if the lab is selected
+    if (isset($_SESSION['labid'])) {
+        echo "<div class=\"mt-5\">";
+        echo "<h2>Questions for the Selected Lab</h2>";
+
+        while ($rowQuestion = $resultQuestions->fetch_assoc()) {
+            echo "<div class=\"card mt-3\">";
+            echo "<div class=\"card-body\">";
+            echo "<h5 class=\"card-title\">Question ID: " . $rowQuestion['QuestionID'] . "</h5>";
+            echo "<p class=\"card-text\">Question Text: " . $rowQuestion['QuestionText'] . "</p>";
+            echo "<p class=\"card-text\">Difficulty Level: " . $rowQuestion['DifficultyLevel'] . "</p>";
+            // Add more details or formatting as needed
+            echo "</div>";
+            echo "</div>";
+        }
+
+        echo "</div>";
+    }
+    ?>
 </div>
+
+<!-- Include Bootstrap JS and jQuery CDN links here if needed -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 
 </body>
 </html>
